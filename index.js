4e63df08a2d51955814765392807b095cb632c9d
efueyo <efueyo@sentisis.com>
2016-01-20 12:46:37 +0100

    unique can be passed to opts

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var slug = require('slug-component');
  *
  *      - `.replace` characters to replace defaulted to `[^a-zA-Z]`
  *      - `.separator` separator to use, defaulted to `-`
+ *      - `.unique` whether the slug is unique or not, detauls to  to `true`
  *      - `required` whether a slug is required, defaults to `true`
  *
  * @param {String} prop
@@ -25,9 +26,12 @@ var slug = require('slug-component');
 module.exports = function(prop, required, opts){
   return (function slugize(schema){
     var required = required||false;
-    var title;
 
-    schema.add({ slug: {type: String, unique:true, required:required} });
+    var title;
+    var slug_opts = {type: String, required:required} ;
+    if (!opts || opts.unique)
+      slug_opts.unique = true
+    schema.add({ slug: slug_opts});
     schema.pre('save', function(next){
       var self = this;
 
@@ -43,6 +47,7 @@ module.exports = function(prop, required, opts){
 
       var require = (opts && !opts.required) ? false : true;
       if (require && !title) return next(new Error(prop + ' is required to create a slug'));
+      delete opts.unique // clean opts
       if (title) self.slug = slug(title, opts);
       next();
     });
